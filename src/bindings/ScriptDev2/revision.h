d06cb6c76658e0add10aa5129905f8a048135cfe
unknown <Laviniu@.(none)>
2010-02-26 00:19:58 +0200

    Signed-off-by: unknown <Laviniu@.(none)>

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1587"
- #define REVISION_DATE "2010-02-21"
- #define REVISION_TIME "12:15:06"
+ #define REVISION_ID "1605"
+ #define REVISION_DATE "2010-02-25"
+ #define REVISION_TIME "12:27:55"
 #endif // __REVISION_H__
