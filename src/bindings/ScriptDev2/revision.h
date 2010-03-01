3fe5314db99e3d321316a6b59238201cc8c3e2a5
unknown <Laviniu@.(none)>
2010-03-01 19:38:40 +0200

    Merge git://github.com/mangos/mangos
    Conflicts:
    	src/mangosd/Makefile.am

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1605"
- #define REVISION_DATE "2010-02-25"
- #define REVISION_TIME "12:27:55"
+ #define REVISION_ID "1606"
+ #define REVISION_DATE "2010-02-27"
+ #define REVISION_TIME "01:34:47"
 #endif // __REVISION_H__
