acc66057345e264cf8d6f9b8ca5594b9f1cec52c
unknown <Laviniu@.(none)>
2010-03-08 23:26:03 +0200

    scriptdev2

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1625"
- #define REVISION_DATE "2010-03-04"
- #define REVISION_TIME "13:17:31"
+ #define REVISION_ID "1637"
+ #define REVISION_DATE "2010-03-08"
+ #define REVISION_TIME "10:09:58"
 #endif // __REVISION_H__
