67384c96f943848f490d4c6db203f2e4c21e0b06
unknown <Laviniu@.(none)>
2010-04-15 20:54:00 +0300

    sd2

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1669"
- #define REVISION_DATE "2010-04-05"
- #define REVISION_TIME "12:14:03"
+ #define REVISION_ID "1675"
+ #define REVISION_DATE "2010-04-11"
+ #define REVISION_TIME "21:47:42"
 #endif // __REVISION_H__
---
+++ src/bindings/ScriptDev2/sql/mine/forge_of_souls.sql
@@ -0,0 +1,7 @@
+INSERT INTO `script_texts` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`, `emote`, `comment`) VALUES
+(-1632001, 'Finally...a captive audience!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16595, 1, 0, 0, 'Bronjham SAY_AGGRO'),
+(-1632002, 'Fodder for the engine!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16596, 1, 0, 0, 'Bronjham SAY_SLAY_1'),
+(-1632003, 'Another soul to strengthen the host!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16597, 1, 0, 0, 'Bronjham SAY_SLAY_2'),
+(-1632004, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16598, 1, 0, 0, 'Bronjham SAY_DEATH'),
+(-1632005, 'The vortex of the harvested calls to you!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16599, 1, 0, 0, 'Bronjham SAY_SOUL_STORM'),
+(-1632006, 'I will sever the soul from your body!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 16600, 1, 0, 0, 'Bronjham SAY_CORRUPT_SOUL');
