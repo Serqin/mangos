00e550106711e3763d6578e1be581e6efeb869a8
unknown <Laviniu@.(none)>
2010-05-17 21:25:54 +0300

    sd2 update

--- src/bindings/ScriptDev2/revision.h
+++ src/bindings/ScriptDev2/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "1683"
- #define REVISION_DATE "2010-05-04"
- #define REVISION_TIME "12:17:06"
+ #define REVISION_ID "1695"
+ #define REVISION_DATE "2010-05-15"
+ #define REVISION_TIME "18:59:59"
 #endif // __REVISION_H__
